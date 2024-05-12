0569aedf0626cef076e57552538fa95fcdcbcb4c
Vladislav <WWolkoW04@yandex.ru>
2024-05-13 00:10:49 +0400

    Added more methods

--- include/graph/graph.h
+++ include/graph/graph.h
@@ -21,8 +21,13 @@ namespace graph {
 			return _data.erase(v);
 		}
 		void print() const {
-			for (auto& elem: _data) {
-				std::cout << elem.first << ' ';
+			for (auto& v: _data) {
+				std::cout << v.first << ": ";
+				for (auto& e: v.second)
+				{
+					std::cout << e.first << "(" << e.second << ") ";
+				}
+				std::cout << std::endl;
 			}
 			std::cout << std::endl;
 		}
@@ -38,6 +43,34 @@ namespace graph {
 			_data[from].push_back(std::make_pair(to, d));
 		}
 
+		bool remove_edge(const Vertex& from, const Vertex& to) {
+			if (!has_vertex(from) || !has_vertex(to)) return false;
+			auto it = _data[from].begin();
+			while (it != _data[from].end() && it->first != to) ++it;
+			if (it == _data[from].end()) return false;
+			_data[from].erase(it);
+			return true;
+		}
+
+		bool has_edge(const Vertex& from, const Vertex& to) const {
+			if (!has_vertex(from) || !has_vertex(to)) return false;
+			auto it = _data.at(from).begin();
+			while (it != _data.at(from).end() && it->first != to) ++it;
+			return it != _data.at(from).end();
+		}
+		std::vector<std::pair<Vertex, Distance>> edges(const Vertex& v) const{
+			std::vector<std::pair<Vertex, Distance>> edges;
+			for (auto& e : _data.at(v)) {
+				edges.push_back(e);
+			}
+			return edges;
+		}
+		size_t degree(const Vertex& v) const {
+			return _data.at(v).size();
+		}
+		size_t order() const {//порядок - кол-во вершин
+			return _data.size();
+		}
 	private:
 		std::unordered_map<Vertex, std::vector<std::pair<Vertex, Distance>>> _data;
 	};
--- tests/graph_tests.cpp
+++ tests/graph_tests.cpp
@@ -4,17 +4,20 @@
 using namespace graph;
 
 TEST(Creating, First) {
-	Graph<int> graph;
+	Graph<int, int> graph;
 	graph.add_vertex(1);
 	graph.add_vertex(2);
 	graph.add_vertex(3);
 	graph.add_vertex(4);
 	graph.add_vertex(5);
 	graph.add_vertex(6);
-	auto vertices = graph.vertices();
-	for (auto& v : vertices)
+	graph.add_edge(1, 2, 10);
+	graph.add_edge(1, 3, 11);
+	graph.add_edge(4, 6, 12);
+	/*auto edges = graph.edges(1);
+	for (auto& e: edges)
 	{
-		std::cout << v << ' ';
-	}
-	graph.add_edge(1, 2, 5.5);
+		std::cout << e.first << "(" << e.second << ")" << " ";
+	}*/
+	std::cout << graph.order() << std::endl;
 }
