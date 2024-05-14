76b0228dbe6fc605c94a47fa89254d9cbeebda9d
Vladislav <WWolkoW04@yandex.ru>
2024-05-15 00:46:19 +0400

    shortest_path and walk

--- include/graph/graph.h
+++ include/graph/graph.h
@@ -3,6 +3,8 @@
 #include<unordered_map>
 #include<vector>
 #include<utility>
+#include<queue>
+#include<stack>
 
 namespace graph {
 	template<class Vertex, class Distance = double>
@@ -21,9 +23,9 @@ namespace graph {
 			return _data.erase(v);
 		}
 		void print() const {
-			for (auto& v: _data) {
+			for (auto& v : _data) {
 				std::cout << v.first << ": ";
-				for (auto& e: v.second)
+				for (auto& e : v.second)
 				{
 					std::cout << e.first << "(" << e.second << ") ";
 				}
@@ -33,7 +35,7 @@ namespace graph {
 		}
 		std::vector<Vertex> vertices() const {
 			std::vector<Vertex> vertices;
-			for(auto& elem: _data) {
+			for (auto& elem : _data) {
 				vertices.push_back(elem.first);
 			}
 			return vertices;
@@ -51,14 +53,21 @@ namespace graph {
 			_data[from].erase(it);
 			return true;
 		}
-
+		/*
+		bool remove_edge(const std::pair<Vertex, Distance>& e) {//c учетом расстояния
+			return true;
+		}*/
 		bool has_edge(const Vertex& from, const Vertex& to) const {
 			if (!has_vertex(from) || !has_vertex(to)) return false;
 			auto it = _data.at(from).begin();
 			while (it != _data.at(from).end() && it->first != to) ++it;
 			return it != _data.at(from).end();
 		}
-		std::vector<std::pair<Vertex, Distance>> edges(const Vertex& v) const{
+		/*
+		bool has_edge(const std::pair<Vertex, Distance>& e) const {//c учетом расстояния в Edge
+			return true
+		}*/
+		std::vector<std::pair<Vertex, Distance>> edges(const Vertex& v) const {
 			std::vector<std::pair<Vertex, Distance>> edges;
 			for (auto& e : _data.at(v)) {
 				edges.push_back(e);
@@ -68,10 +77,71 @@ namespace graph {
 		size_t degree(const Vertex& v) const {
 			return _data.at(v).size();
 		}
-		size_t order() const {//порядок - кол-во вершин
-			return _data.size();
+		size_t order() const {
+			return _data.size();//порядок - кол-во вершин
+		}
+
+		std::vector<Vertex> walk(const Vertex& start_vertex){
+			std::unordered_map<Vertex, bool> visited;
+			std::vector<Vertex> walk;
+			/*for (size_t i = 0; i < _data.size(); ++i)
+			{
+				visited[i] = false;
+			}*/
+			std::queue<Vertex> queue;
+			queue.push(start_vertex);
+			while (!queue.empty()) {
+				Vertex u = queue.front();
+				walk.push_back(u);
+				queue.pop();
+				for (const auto& v : _data[u]) {
+					if (!visited[v.first]) {
+						visited[v.first] = true;
+						queue.push(v.first);
+					}
+				}
+			}
+			return walk;
+		}
+
+		std::vector<std::pair<Vertex, Distance>> shortest_path(const Vertex& from, const Vertex& to)  {
+			std::unordered_map<Vertex, std::pair<Distance, Vertex>> dist_pred;
+			std::vector<std::pair<Distance, Vertex>> path;
+			std::vector<Vertex> vertices = this->vertices();
+			for (auto& e : vertices) {
+				dist_pred[e] = std::make_pair(1e9, -1);
+			}
+			dist_pred[from] = std::make_pair(0, -1);
+			std::stack<int> stack;
+			stack.push(from);
+			while (!stack.empty()) {
+				Vertex u = stack.top();
+				stack.pop();
+				for (auto& v: _data.at(u)){
+					stack.push(v.first);
+					relax(u, v.first, dist_pred);
+				}
+			}
+			Vertex finish = to;
+			std::pair<Distance, Vertex> pred = dist_pred.at(finish);
+			while (finish != from) {
+				path.insert(path.begin(), std::make_pair(pred.first, finish));
+				finish = pred.second;
+				pred = dist_pred.at(finish);
+			}
+			path.insert(path.begin(), std::make_pair(0, from));
+			return path;
 		}
 	private:
+		void relax(const Vertex& u, const Vertex& v, std::unordered_map<Vertex, 
+			std::pair<Distance, Vertex>>& dist_pred) {
+			auto from_u = _data.at(u).begin();
+			while (from_u->first != v) ++from_u;
+			if (dist_pred.at(v).first > dist_pred.at(u).first + from_u->second) {
+				dist_pred.at(v).first = dist_pred.at(u).first + from_u->second;
+				dist_pred.at(v).second = u;
+			}
+		}
 		std::unordered_map<Vertex, std::vector<std::pair<Vertex, Distance>>> _data;
 	};
 }
--- tests/graph_tests.cpp
+++ tests/graph_tests.cpp
@@ -5,19 +5,28 @@ using namespace graph;
 
 TEST(Creating, First) {
 	Graph<int, int> graph;
+	graph.add_vertex(0);
 	graph.add_vertex(1);
 	graph.add_vertex(2);
 	graph.add_vertex(3);
 	graph.add_vertex(4);
 	graph.add_vertex(5);
-	graph.add_vertex(6);
-	graph.add_edge(1, 2, 10);
-	graph.add_edge(1, 3, 11);
-	graph.add_edge(4, 6, 12);
-	/*auto edges = graph.edges(1);
-	for (auto& e: edges)
-	{
-		std::cout << e.first << "(" << e.second << ")" << " ";
-	}*/
-	std::cout << graph.order() << std::endl;
+	graph.add_edge(0, 2, 7);
+	graph.add_edge(0, 5, 5);
+	graph.add_edge(5, 4, 10);
+	graph.add_edge(2, 3, 1);
+	graph.add_edge(3, 4, 4);
+	graph.add_edge(4, 1, 16);
+	/*int from = 0;
+	int to = 1;
+	auto path = graph.shortest_path(from, to);
+	for (auto& v : path) {
+		std::cout << v.second << "(" << v.first << ")" << " ";
+	}
+	std::cout << std::endl;*/
+	auto walk = graph.walk(0);
+	for (auto& v : walk) {
+		std::cout << v << " ";
+	}
+	std::cout << std::endl;
 }
